d84ae7149142d428955da22f6dc8d69a7c9cba88
Srikant Sarwa <ssarwa@microsoft.com>
2019-11-06 14:44:00 -0800

    Initial commit

---
+++ simple-sr-rest-api/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace simple_sr_rest_api.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
